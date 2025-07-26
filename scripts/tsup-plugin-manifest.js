9b97fe8ac7b16d14aa724675c164c6aac59e353b
Ziggle <no-reply@ziggle.dev>
2025-07-26 14:55:40 -0400

    chore: remove obsolete scripts
    - Remove bundle-builtin-tools.js (outdated bundling approach with hardcoded metadata)
    - Remove generate-core-tools.js (hardcoded path to /Users/james/Projects/)
    - Remove tsup-plugin-manifest.js (unused wrapper)
    
    Keep essential scripts:
    - generate-tool-manifest.js (used in build process)
    - validate-tool-submission.js (needed for package manager system)

--- scripts/tsup-plugin-manifest.js
+++
@@ -1,8 +0,0 @@
-import { generateToolManifest } from './generate-tool-manifest.js';
-
-export const manifestPlugin = {
-  name: 'tool-manifest',
-  async buildEnd() {
-    await generateToolManifest();
-  }
-};
